b19d9454b9401d813e1d4fbcd27a2349df63f781
Hamis95 <64984528+Hamis95@users.noreply.github.com>
2020-11-06 00:04:34 +0200

    Add files via upload

---
+++ randomPeli.py
@@ -0,0 +1,6 @@
+import random
+
+items = ['Fall Guys', 'GTA Online', 'Overwatch', 'Trials Evolution', 'Worms Revolution', 'Battlegrounds', 'Portal 2', 'Dota Underlords', "Don't Starve", 'Apex Legends', 'Rocket League', 'Spider Pasianssi', 'CSGO']
+randomPeli = random.choice(items)
+
+print('Pelaatte', randomPeli , 'inisemättä!')
