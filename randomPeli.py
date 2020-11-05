e27d69822df66e54d252837be54ff5233830b799
Hamis95 <64984528+Hamis95@users.noreply.github.com>
2020-11-06 00:06:36 +0200

    Update randomPeli.py

--- randomPeli.py
+++ randomPeli.py
@@ -1,6 +1,6 @@
 import random
 
 items = ['Fall Guys', 'GTA Online', 'Overwatch', 'Trials Evolution', 'Worms Revolution', 'Battlegrounds', 'Portal 2', 'Dota Underlords', "Don't Starve", 'Apex Legends', 'Rocket League', 'Spider Pasianssi', 'CSGO']
-randomPeli = random.choice(items)
+randomPeli = random.choice(items) #just add the items or in this case games here
 
 print('Pelaatte', randomPeli , 'inisemättä!')
